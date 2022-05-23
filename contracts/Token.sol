d4e61cb6a3b986b7cd203378f69d2f1221eb3263
Marlon Hanks <owner@kedia.co>
2022-05-23 00:47:32 -0400

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.7.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-		constructor(uint256 initialBalance) public ERC20("Reef", "REEF") {
+		constructor(uint256 initialBalance) public ERC20("Dust", "DUST") {
 				_mint(msg.sender, initialBalance);
 		}
 }
